70d601fbcb19f503f91166e954749899c64e35aa
David Gieser <dpgieser@gmail.com>
2023-03-10 23:59:56 -0600

    Added Download Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import * as API from './api/API'
 import './App.css'
 
 function App(): JSX.Element {
   const [file, setFile] = useState<any>()
+  const downloadRef = useRef<HTMLInputElement>(null)
 
   const handleFileChange = (data: any): void => {
     setFile(data.target.files[0])
   }
 
-  const onSubmit = async (event: any): Promise<void> => {
+  const onUploadSubmit = async (event: any): Promise<void> => {
     event.preventDefault()
     const formData = new FormData()
     formData.append('file', file)
@@ -21,12 +22,29 @@ function App(): JSX.Element {
     }
   }
 
+  const onDownloadSubmit = async (_: any): Promise<void> => {
+    var formData
+    if (downloadRef.current != null) {
+      formData = JSON.stringify({"FileName" : downloadRef.current.value})
+    }
+
+    const res: any = await fetch('https://127.0.0.1:8080/download', {
+      method: 'GET',
+      body: formData,
+    }).then(async (res) => await res.json())
+    console.log(res)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         QuickShare
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onUploadSubmit}>
           <input name="file_upload" type="file" onChange={handleFileChange}></input>
+          <input type="Submit"></input>
+        </form>
+        <form onSubmit={onDownloadSubmit}>
+          <input name="file_download" type="text" ref={downloadRef}></input>
           <input type="Submit"></input>
         </form>
       </header>
