89f77bdbfd9f7774c160a053035e36908a084c66
Amit Prasad <17amitprasad@gmail.com>
2023-04-13 16:24:51 -0500

    SPA-ify and Custom IDs in frontend

--- frontend/components/upload-form.tsx
+++ frontend/components/upload-form.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react"
 
 import API, { API_URL } from "@/lib/api"
-
-import type { UploadResponse } from "@/lib/types"
 import CheckSum from "@/lib/checksum"
-
-import { Progress } from "@/components/ui/progress"
-import CopyButton from "@/components/copy-button"
+import type { UploadResponse } from "@/lib/types"
 import { cn } from "@/lib/utils"
+import CopyButton from "@/components/copy-button"
 import { FileInput } from "@/components/drag-drop-file-input"
 import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
 import { Switch } from "@/components/ui/switch"
 
 // Form for uploading files to the backend
@@ -29,9 +27,11 @@ const UploadForm = (): JSX.Element => {
     }
 
     const file = e.target.files[0]
-    const { name } = file // Extract filename
-    setFilename(name)
     setUploadFile(file)
+  }
+
+  const handleIDChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setFilename(e.target.value)
   }
 
   const handleExpiryChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -45,10 +45,13 @@ const UploadForm = (): JSX.Element => {
   }
 
   const onUploadSubmit = async (event: any): Promise<void> => {
+    if (uploadFile === undefined) {
+      return
+    }
     setFileId("")
     setHash("")
     setProgress(0)
-    
+
     // Calculate the hash of the file
     CheckSum(uploadFile, setHash, setProgress)
 
@@ -56,6 +59,9 @@ const UploadForm = (): JSX.Element => {
     const formData = new FormData()
     formData.append("file", uploadFile)
     formData.append("expiration", expiryEnabled ? expiry : "")
+    if (filename !== "") {
+      formData.append("custom_id", filename)
+    }
     try {
       const res = await API.upload(formData)
       setFileId(res.response.file_id)
@@ -75,31 +81,46 @@ const UploadForm = (): JSX.Element => {
         </span>
         <FileInput type="file" onChange={handleFileChange} id="file-upload" />
       </label>
-      <label
-        htmlFor="enable-expiry"
-        className="text-sm text-gray-600 dark:text-gray-400 flex gap-2 items-center"
-      >
-        <span className="mt-2 mb-2 text-base leading-normal">
-          Enable file expiry
-        </span>
-        <Switch onCheckedChange={toggleExpiryEnabled} id="enable-expiry" />
-      </label>
-      <label
-        htmlFor="file-expiry"
-        className={cn(
-          "text-sm text-gray-600 dark:text-gray-400",
-          expiryEnabled ? "" : "hidden"
-        )}
-      >
-        <span className="mt-2 mb-2 text-base leading-normal">
-          Select expiry time
-        </span>
-        <Input
-          type="datetime-local"
-          onChange={handleExpiryChange}
-          id="file-expiry"
-        />
-      </label>
+      <div className="flex flex-row gap-2 w-full justify-between">
+        <div className="flex-1">
+          <label
+            htmlFor="enable-expiry"
+            className="text-sm text-gray-600 dark:text-gray-400 flex gap-2 items-center"
+          >
+            <span className="mt-2 mb-2 text-base leading-normal">
+              Enable file expiry
+            </span>
+            <Switch onCheckedChange={toggleExpiryEnabled} id="enable-expiry" />
+          </label>
+          <label
+            htmlFor="file-expiry"
+            className={cn(
+              "text-sm text-gray-600 dark:text-gray-400",
+              expiryEnabled ? "" : "hidden"
+            )}
+          >
+            <span className="mt-2 mb-2 text-base leading-normal">
+              Select expiry time
+            </span>
+            <Input
+              type="datetime-local"
+              onChange={handleExpiryChange}
+              id="file-expiry"
+            />
+          </label>
+        </div>
+        <div className="flex-1">
+          <label
+            htmlFor="file-expiry"
+            className="text-sm text-gray-600 dark:text-gray-400"
+          >
+            <span className="mt-2 mb-2 text-base leading-normal">
+              Upload ID (optional)
+            </span>
+            <Input type="text" onChange={handleIDChange} id="file-expiry" />
+          </label>
+        </div>
+      </div>
       <label
         htmlFor="file-submit"
         className="text-sm text-gray-600 dark:text-gray-400"
@@ -110,16 +131,22 @@ const UploadForm = (): JSX.Element => {
       {fileId !== "" && fileHash !== "" ? (
         <div className="mt-4">
           File uploaded successfully. <br />
-          ID:{" "}  
-          <a href={`${API_URL}/download/${fileId}`} className="text-blue-500" data-testid="file_id">
-            {fileId} 
-          </a> <CopyButton fileInfo={fileId}/>  <br /> 
-          Hash: {fileHash} <CopyButton fileInfo={fileHash}/>
+          ID:{" "}
+          <a
+            href={`${API_URL}/download/${fileId}`}
+            className="text-blue-500"
+            data-testid="file_id"
+          >
+            {fileId}
+          </a>{" "}
+          <CopyButton fileInfo={fileId} /> <br />
+          Hash: {fileHash} <CopyButton fileInfo={fileHash} />
         </div>
-      ) : 
-      <div>
-        <br /> <Progress value={hashProgress}/>
-      </div>}
+      ) : (
+        <div>
+          <br /> <Progress value={hashProgress} />
+        </div>
+      )}
     </div>
   )
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,30 @@
 import Head from "next/head"
 
+import DownloadForm from "@/components/download-form"
 import { Layout } from "@/components/layout"
+import { Button } from "@/components/ui/button"
+import UploadForm from "@/components/upload-form"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 export default function IndexPage() {
+  const setPage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const isUpload = e.currentTarget.id === "upload-button"
+    const uploadButton = document.getElementById("upload-button")! as HTMLButtonElement
+    const downloadButton = document.getElementById("download-button")! as HTMLButtonElement
+    if (isUpload) {
+      console.log("Upload")
+      document.getElementById("upload-form")!.classList.remove("hidden")
+      document.getElementById("download-form")!.classList.add("hidden")
+      uploadButton.disabled = true
+      downloadButton.disabled = false
+    } else {
+      console.log("Download")
+      document.getElementById("upload-form")!.classList.add("hidden")
+      document.getElementById("download-form")!.classList.remove("hidden")
+      downloadButton.disabled = true
+      uploadButton.disabled = false
+    }
+  }
   return (
     <Layout>
       <Head>
@@ -11,10 +33,27 @@ export default function IndexPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <div className="flex max-w-[980px] gap-2 items-center justify-between">
           <h1 className="text-3xl leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            File sharing
+            QuickShare
           </h1>
+          <ThemeToggle/>
+        </div>
+        <div className="flex w-full justify-center gap-4">
+          <Button id="upload-button" className="flex-1" onClick={setPage}>
+            Upload
+          </Button>
+          <Button id="download-button" className="flex-1" onClick={setPage}>
+            Download
+          </Button>
+        </div>
+        <div className="hidden" id="upload-form">
+          <h2 className="text-2xl">Upload a file</h2>
+          <UploadForm />
+        </div>
+        <div className="hidden" id="download-form">
+          <h2 className="text-2xl">Download a file</h2>
+          <DownloadForm />
         </div>
         <div className="flex gap-4"></div>
       </section>
