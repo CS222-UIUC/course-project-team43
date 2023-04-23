698bbb74faa5f25b84249c14b2f046cff451f7e0
Amit Prasad <17amitprasad@gmail.com>
2023-04-23 16:58:36 -0500

    Download errors

--- frontend/components/download-form.tsx
+++ frontend/components/download-form.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react"
 
-import API from "@/lib/api"
+import API, { APIError } from "@/lib/api"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Input } from "@/components/ui/input"
+
+enum Status {
+  Nothing,
+  Done,
+  Error,
+}
 
 // Form for uploading files to the backend
 const DownloadForm = (): JSX.Element => {
   const [filename, setFileName] = useState<string>("")
 
+  const [status, setStatus] = useState<Status>(Status.Nothing)
+  const [downloadError, setDownloadError] = useState<string>("")
+
   const onDownloadSubmit = async (_: any): Promise<void> => {
     if (filename !== "") {
       try {
-        let res = await API.download(filename);
-        const url = window.URL.createObjectURL(res.response); // res.response is a blob
+        let res = await API.download(filename)
+        const url = window.URL.createObjectURL(res.response) // res.response is a blob
         const a = document.createElement("a")
         a.href = url
         a.download = `${filename}.txt`
@@ -19,8 +29,14 @@ const DownloadForm = (): JSX.Element => {
         a.click()
         a.remove()
         window.URL.revokeObjectURL(url)
+        setStatus(Status.Done)
       } catch (err) {
-        console.log(err)
+        setStatus(Status.Error)
+        if (err instanceof APIError) {
+          setDownloadError(err.message)
+        } else {
+          setDownloadError("An unknown error occurred")
+        }
       }
     }
   }
@@ -47,6 +63,19 @@ const DownloadForm = (): JSX.Element => {
         <span className="mt-2 mb-2 text-base leading-normal">Download</span>
         <Input type="submit" onClick={onDownloadSubmit} id="file-download" />
       </label>
+      {status === Status.Done ? (
+        <Alert className="mt-4">
+          <AlertTitle>Download Successful</AlertTitle>
+          <AlertDescription>Fetched file from server</AlertDescription>
+        </Alert>
+      ) : (
+        status === Status.Error && (
+          <Alert className="mt-4" variant="destructive">
+            <AlertTitle>Upload Failed</AlertTitle>
+            <AlertDescription>{downloadError}</AlertDescription>
+          </Alert>
+        )
+      )}
     </div>
   )
 }
